21be595eae4e1dc555c99b9864a81799b6f16f51
karan bhatia <karan.bhatia@gmail.com>
2022-11-18 16:36:33 -0500

    updated docs style

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,21 +1,48 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
 import os
 import sys
-# Need this so sphinx can find lumache.py. Change is .py files are elsewhere than root.
+import alabaster
+
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../src'))
 
-# -- Project information
+# -- Project information -----------------------------------------------------
 
-project = 'Coastal'
-copyright = '2021'
-author = 'Multiple'
+project = "project name"
+copyright = "2022, Google LLC."
+author = "Google Cloud Healthcare and Life Sciences"
 
-release = '0.1'
-version = '0.1.0'
 
-# -- General configuration
+# -- General configuration ---------------------------------------------------
 
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
@@ -23,60 +50,90 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'autoapi.extension',
-#    'sphinxcontrib.apidoc',
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx_copybutton",
+    "sphinxcontrib.httpdomain",
+    "sphinxcontrib.kroki",
+    "alabaster",
+    "releases",
 ]
 
-autoapi_type = 'python'
-autoapi_dirs = ['../../src']
+autodoc_mock_imports = [
+]
 
-#apidoc_module_dir = '../../src'
-#apidoc_output_dir = '.'
-#apidoc_excluded_paths = ['tests']
-#apidoc_separate_modules = True
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
+autoclass_content = "both"  # Add __init__ doc (ie. params) to class summaries
+html_show_sourcelink = (
+    True  # add 'view source code' from top of page (for html, not python)
+)
+autodoc_inherit_docstrings = True  # If no docstring, inherit from base class
+set_type_checking_flag = (
+    True  # Enable 'expensive' imports for sphinx_autodoc_typehints
+)
+nbsphinx_allow_errors = True  # Continue through Jupyter errors
+# autodoc_typehints = "description" # Sphinx-native method. Not as good as sphinx_autodoc_typehints
+add_module_names = False  # Remove namespaces from class/method signatures
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
 
-templates_path = ['_templates']
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
 
-# -- Options for HTML output
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_theme = 'sphinx_rtd_theme'
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
-
-html_context = {
-    "display_github": True, # Integrate GitHub
-    "github_repo": "eeholmes/readthedoc-test", # Repo name
-    "github_version": "main", # Version
-    "conf_py_path": "docs/source/", # Path in the checkout to the docs root
-}
 
 # -- Options for HTML output -------------------------------------------------
 
-html_short_title = "topobathy"
-html_show_sourcelink = False
-html_show_sphinx = True
-html_show_copyright = True
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme_path = [alabaster.get_path()]
+html_theme = "alabaster"
+
+html_sidebars = {
+    "**": [
+        "about.html",
+        "navigation.html",
+        "relations.html",
+        "searchbox.html",
+    ]
+}
+html_theme_options = {
+    "description": "Project Description",
+    "github_button": False,
+    "github_user": "lila",
+    "github_repo": "alabaster",
+    "fixed_sidebar": True,
+    "logo": "logo.png",
+    "logo_name": True,
+    "show_powered_by": False,
+}
+
+releases_github_path = "bitprophet/alabaster"
+# Our pre-0.x releases are unstable / mix bugs+features
+releases_unstable_prehistory = True
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-repository_url = f"https://github.com/ICESAT-2HackWeek/coastal_topobathy"
-html_context = {
-    "menu_links": [
-        (
-            '<i class="fa fa-github fa-fw"></i> Source Code',
-            repository_url,
-        ),
-        (
-            '<i class="fa fa-book fa-fw"></i> License',
-            f"{repository_url}/blob/main/LICENSE",
-        ),
-    ],
-}
+html_static_path = ["_static"]
